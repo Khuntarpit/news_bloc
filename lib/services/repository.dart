80fbb2303ff7b5607689f546b80091038a21e0eb
khuntbrothers <arpitkhunt66@gmail.com>
2022-09-04 21:48:53 +0530

    remove repository

--- lib/services/repository.dart
+++ lib/services/repository.dart
@@ -1,8 +1,18 @@
+import 'package:news_bloc/Utils/Constants.dart';
 import 'package:news_bloc/models/NewsApiResonse/article.dart';
-
-import 'newsApiProvider.dart';
+import 'package:news_bloc/services/http_service.dart';
+import 'package:news_bloc/utils/AppUrls.dart';
 
 class Repository {
-  final moviesApiProvider = NewsApiProvider();
-  Future<List<Article>> fetchAllNews({String category = ''}) => moviesApiProvider.fetchNewsList(category:category);
+  HttpService httpService = HttpService();
+
+  Future<List<Article>> fetchAllNews({String category = ''}) async {
+    final response = await httpService.getRequest(AppUrls.topHeadLineUrl(category));
+    if (checkResponse(response.statusCode)) {
+      List<Article> articles = List<Article>.from(response.data["articles"].map((x) => Article.fromJson(x)));
+      return articles;
+    } else {
+      return [];
+    }
+  }
 }
